7ee4ef33354771da2c17c1da69fcfdee8d62d7f2
developersatish <satishsingh.net@outlook.com>
2022-01-27 18:24:48 +0530

    new change

--- src/component/GitHub.js
+++ src/component/GitHub.js
@@ -6,22 +6,28 @@ import react, { useState, useEffect } from "react";
 
 function GitHub(props) {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
+        if (!props.name) return;
+        setLoading(true);
         fetch(`https://api.github.com/users/${props.name}`)
             .then((re) => re.json())
-            .then(setUser);
-    }, []);
-    if (user) {
-        return <div>
-            <h1>{user.login}</h1>
-            <img src={user.avatar_url}/>
-        </div>;
-    } else {
-        return <div className="App">
-            NO DATA FOUND!
-        </div>;
-    }
+            .then(setUser)
+            .then(setLoading(false))
+            .catch(setError);
 
+    }, [props.name]);
+    if (loading) return <h1>Loading..</h1>;
+    if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+    if (!user) return null;
+
+    return (<div className="App">
+        <h1>{user?.login}</h1>
+        <h2>{user?.name}</h2>
+        <img src={user?.avatar_url} alt={user?.location} />
+    </div>);
 }
 
 export default GitHub;
